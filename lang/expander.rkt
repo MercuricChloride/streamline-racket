5c4a9549116c6cd9c168a056604b150ea683f453
mercuricchloride <mercuryprotocol@protonmail.com>
2024-01-23 11:43:38 -0800

    Added whole body readers and now lang is working

--- lang/expander.rkt
+++ lang/expander.rkt
@@ -1,24 +1,27 @@
 #lang racket
 
 (require (prefix-in parser: streamline/lang/parser)
+         (prefix-in r: racket)
          ;; We need to import the syntax-classes, from another file
          ;; at compile time, but the syntax classes are defined at run time
-         (for-syntax syntax/parse "./classes.rkt"))
+         (for-syntax syntax/parse))
 
 ;;; PARAMS FOR THE MODULE
 (define import-list (make-parameter '()))
 
-(define-syntax-rule (@%module-begin node ...) (#%module-begin node ...))
+(define-syntax-rule (@%module-begin node ...) (r:#%module-begin node ...))
 
 (define-syntax (streamline-datum stx)
   (syntax-parse stx
-    [(_ . #s(source-def path:expr)) #'(import-list (cons path (import-list)))]
-    [(_ . v) #'(#%datum v)]))
+    [(_ . #s(source-def path:expr)) #'(import-list (r:cons path (import-list)))]
+    [(_ . v)
+     #'(begin
+         (r:print "hi")
+         (r:#%datum v))]))
 
 (define (streamline:read-syntax path input)
-  (define ast (parser:parse-streamline! input))
-  #`(module streamline-module streamline
-      #,ast))
+  (define ast (syntax->list (parser:parse-streamline! input)))
+  ast)
 
 (define (streamline:read in)
   (syntax->datum (streamline:read-syntax "" in)))
@@ -27,12 +30,11 @@
   (syntax-parse stx
     [(_ import-list) #'(import-list)]))
 
-(provide (rename-out (streamline:read read)
-                     (streamline:read-syntax read-syntax)
-                     (streamline-datum #%datum)
-                     (@%module-begin #%module-begin)
-                     (@%app #%app))
+(provide (rename-out (streamline-datum #%datum) (@%module-begin #%module-begin) (@%app #%app))
          #%top
          #%top-interaction
-         provide
+         streamline:read-syntax
+         streamline:read
          import-list)
+
+;;(streamline:read-syntax "asdf" (open-input-file "../examples/simpleErc721.strm"))
